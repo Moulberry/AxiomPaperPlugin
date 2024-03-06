c4b51b0e7eeb6b09fa51651bb59c864788bc1ba3
Moulberry <james.jenour@protonmail.com>
2024-03-06 16:21:27 +0800

    Adapt version util for 1.20.1

--- src/main/java/com/moulberry/axiom/VersionHelper.java
+++ src/main/java/com/moulberry/axiom/VersionHelper.java
@@ -1,14 +1,15 @@
 package com.moulberry.axiom;
 
-import com.moulberry.axiom.packet.CustomByteArrayPayload;
-import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
 public class VersionHelper {
 
     public static void sendCustomPayload(ServerPlayer serverPlayer, ResourceLocation id, byte[] data) {
-        serverPlayer.connection.send(new ClientboundCustomPayloadPacket(new CustomByteArrayPayload(id, data)));
+        serverPlayer.connection.send(new ClientboundCustomPayloadPacket(id, new FriendlyByteBuf(Unpooled.wrappedBuffer(data))));
     }
 
 }
