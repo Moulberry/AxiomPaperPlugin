17693944818d8f8949e82bb946b36b8922ad12c5
Moulberry <james.jenour@protonmail.com>
2024-05-19 16:13:40 +0800

    Clean up CoreProtect integration

--- src/main/java/com/moulberry/axiom/integration/coreprotect/CoreProtectIntegration.java
+++ src/main/java/com/moulberry/axiom/integration/coreprotect/CoreProtectIntegration.java
@@ -9,52 +9,21 @@ public class CoreProtectIntegration {
     public static boolean isEnabled() {
         return CoreProtectIntegrationImpl.isEnabled();
     }
-    
-    public static boolean logPlacement(String name, BlockState blockState, CraftWorld world, BlockPos pos) {
+
+    public static void logPlacement(String name, BlockState blockState, CraftWorld world, BlockPos pos) {
         if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
+            return;
         }
 
-        return CoreProtectIntegrationImpl.logPlacement(name, blockState, world, pos);
+        CoreProtectIntegrationImpl.logPlacement(name, blockState, world, pos);
     }
 
-    public static boolean logPlacement(String name, BlockState blockState, CraftWorld world, int x, int y, int z) {
+    public static void logRemoval(String name, BlockState blockState, CraftWorld world, BlockPos pos) {
         if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
-        }
-        
-        return CoreProtectIntegrationImpl.logPlacement(name, blockState, world, x, y, z);
-    }
-
-    public static boolean logPlacement(String name, Level level, CraftWorld world, BlockPos pos) {
-        if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
+            return;
         }
 
-        return CoreProtectIntegrationImpl.logPlacement(name, level, world, pos);
+        CoreProtectIntegrationImpl.logRemoval(name, blockState, world, pos);
     }
 
-    public static boolean logRemoval(String name, BlockState blockState, CraftWorld world, BlockPos pos) {
-        if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
-        }
-
-        return CoreProtectIntegrationImpl.logRemoval(name, blockState, world, pos);
-    }
-
-    public static boolean logRemoval(String name, BlockState blockState, CraftWorld world, int x, int y, int z) {
-        if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
-        }
-
-        return CoreProtectIntegrationImpl.logRemoval(name, blockState, world, x, y, z);
-    }
-
-    public static boolean logRemoval(String name, Level level, CraftWorld world, BlockPos pos) {
-        if (!CoreProtectIntegrationImpl.isEnabled()) {
-            return false;
-        }
-
-        return CoreProtectIntegrationImpl.logRemoval(name, level, world, pos);
-    }
 }
