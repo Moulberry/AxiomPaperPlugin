0bce28dc8d9d112e8c098afb8cc2337fbca8c4ff
Moulberry <james.jenour@protonmail.com>
2024-11-16 21:43:47 +0800

    Update to 1.20.2

--- src/main/java/com/moulberry/axiom/VersionHelper.java
+++ src/main/java/com/moulberry/axiom/VersionHelper.java
@@ -1,6 +1,7 @@
 package com.moulberry.axiom;
 
 import io.netty.buffer.Unpooled;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.common.custom.DiscardedPayload;
 import net.minecraft.resources.ResourceLocation;
@@ -10,6 +11,12 @@ public class VersionHelper {
 
     public static void sendCustomPayload(ServerPlayer serverPlayer, ResourceLocation id, byte[] data) {
         serverPlayer.connection.send(new ClientboundCustomPayloadPacket(new DiscardedPayload(id, Unpooled.wrappedBuffer(data))));
+    }
+
+    public static void sendCustomPayload(ServerPlayer serverPlayer, ResourceLocation id, FriendlyByteBuf friendlyByteBuf) {
+        byte[] data = new byte[friendlyByteBuf.writerIndex()];
+        friendlyByteBuf.getBytes(friendlyByteBuf.readerIndex(), data);
+        sendCustomPayload(serverPlayer, id, data);
     }
 
     public static ResourceLocation createResourceLocation(String composed) {
