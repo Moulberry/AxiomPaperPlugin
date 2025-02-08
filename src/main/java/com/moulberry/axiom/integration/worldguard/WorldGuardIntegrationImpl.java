681681a4195ee89e601b17c99f29645a27ea6668
PSJahn <66626848+PSJahn@users.noreply.github.com>
2025-02-08 19:07:20 +0100

    Added global region to section checks

--- src/main/java/com/moulberry/axiom/integration/worldguard/WorldGuardIntegrationImpl.java
+++ src/main/java/com/moulberry/axiom/integration/worldguard/WorldGuardIntegrationImpl.java
@@ -1,5 +1,6 @@
 package com.moulberry.axiom.integration.worldguard;
 
+import com.google.common.collect.Iterables;
 import com.moulberry.axiom.integration.Box;
 import com.moulberry.axiom.integration.BoxWithBoolean;
 import com.moulberry.axiom.integration.SectionPermissionChecker;
@@ -9,7 +10,6 @@ import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.FlagValueCalculator;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
@@ -17,9 +17,10 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.*;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class WorldGuardIntegrationImpl {
 
@@ -54,22 +55,18 @@ public class WorldGuardIntegrationImpl {
         int minX = cx * 16;
         int minY = cy * 16;
         int minZ = cz * 16;
-        int maxX = cx * 16 + 15;
-        int maxY = cy * 16 + 15;
-        int maxZ = cz * 16 + 15;
 
         WorldGuardPlatform platform = WorldGuard.getInstance().getPlatform();
-
-        RegionContainer regionContainer = platform.getRegionContainer();
-        if (regionContainer == null) {
-            return SectionPermissionChecker.ALL_ALLOWED;
-        }
-
-        com.sk89q.worldedit.world.World worldEditWorld = BukkitAdapter.adapt(world);
         LocalPlayer worldGuardPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+        com.sk89q.worldedit.world.World worldEditWorld = BukkitAdapter.adapt(world);
 
         // Don't do any protection if player has bypass
         if (platform.getSessionManager().hasBypass(worldGuardPlayer, worldEditWorld)) {
+            return SectionPermissionChecker.ALL_ALLOWED;
+        }
+
+        RegionContainer regionContainer = platform.getRegionContainer();
+        if (regionContainer == null) {
             return SectionPermissionChecker.ALL_ALLOWED;
         }
 
@@ -78,10 +75,14 @@ public class WorldGuardIntegrationImpl {
             return SectionPermissionChecker.ALL_ALLOWED;
         }
 
-        BlockVector3 min = BlockVector3.at(cx*16, cy*16, cz*16);
-        BlockVector3 max = BlockVector3.at(cx*16+15, cy*16+15, cz*16+15);
+        BlockVector3 min = BlockVector3.at(cx * 16, cy * 16, cz * 16);
+        BlockVector3 max = BlockVector3.at(cx * 16 + 15, cy * 16 + 15, cz * 16 + 15);
         ProtectedRegion test = new ProtectedCuboidRegion("dummy", min, max);
-        ApplicableRegionSet regions = regionManager.getApplicableRegions(test, RegionQuery.QueryOption.COMPUTE_PARENTS);
+        Iterable<ProtectedRegion> regions = regionManager.getApplicableRegions(test, RegionQuery.QueryOption.COMPUTE_PARENTS).getRegions();
+        ProtectedRegion globalRegion = regionManager.getRegion("__global__");
+        if (globalRegion != null) {
+            regions = Iterables.concat(regions, Collections.singletonList(globalRegion));
+        }
 
         Box sectionBox = new Box(0, 0, 0, 15, 15, 15);
         int lastPriority = Integer.MIN_VALUE;
@@ -103,40 +104,39 @@ public class WorldGuardIntegrationImpl {
             }
 
             StateFlag.State value = FlagValueCalculator.getEffectiveFlagOf(region, Flags.BUILD, worldGuardPlayer);
-            if (value != null) {
-                if (region instanceof GlobalProtectedRegion) {
-                    if (value == StateFlag.State.DENY) {
-                        if (finalRegions.isEmpty()) {
-                            return SectionPermissionChecker.NONE_ALLOWED;
-                        } else {
-                            denyRegions.add(sectionBox);
-                        }
-                    } else if (value == StateFlag.State.ALLOW) {
-                        allowRegions.add(sectionBox);
-                    }
-                } else if (region instanceof ProtectedCuboidRegion || value == StateFlag.State.DENY) {
-                    // Note: we do this for DENY, even if the
-                    // type isn't a cuboid region simply to
-                    // be cautious
+            if (value == null) continue;
 
-                    BlockVector3 regionMin = region.getMinimumPoint();
-                    BlockVector3 regionMax = region.getMaximumPoint();
-
-                    int regionMinX = Math.max(regionMin.getBlockX(), cx*16) - minX;
-                    int regionMinY = Math.max(regionMin.getBlockY(), cy*16) - minY;
-                    int regionMinZ = Math.max(regionMin.getBlockZ(), cz*16) - minZ;
-                    int regionMaxX = Math.min(regionMax.getBlockX(), cx*16+15) - minX;
-                    int regionMaxY = Math.min(regionMax.getBlockY(), cy*16+15) - minY;
-                    int regionMaxZ = Math.min(regionMax.getBlockZ(), cz*16+15) - minZ;
-
-                    Box box = new Box(regionMinX, regionMinY, regionMinZ, regionMaxX, regionMaxY, regionMaxZ);
-                    if (value == StateFlag.State.DENY) {
-                        denyRegions.add(box);
+            if (region instanceof GlobalProtectedRegion) {
+                if (value == StateFlag.State.DENY) {
+                    if (finalRegions.isEmpty()) {
+                        return SectionPermissionChecker.NONE_ALLOWED;
                     } else {
-                        allowRegions.add(box);
+                        denyRegions.add(sectionBox);
                     }
+                } else if (value == StateFlag.State.ALLOW) {
+                    allowRegions.add(sectionBox);
                 }
-                continue;
+            } else if (region instanceof ProtectedCuboidRegion || value == StateFlag.State.DENY) {
+                // Note: we do this for DENY, even if the
+                // type isn't a cuboid region simply to
+                // be cautious
+
+                BlockVector3 regionMin = region.getMinimumPoint();
+                BlockVector3 regionMax = region.getMaximumPoint();
+
+                int regionMinX = Math.max(regionMin.getBlockX(), cx * 16) - minX;
+                int regionMinY = Math.max(regionMin.getBlockY(), cy * 16) - minY;
+                int regionMinZ = Math.max(regionMin.getBlockZ(), cz * 16) - minZ;
+                int regionMaxX = Math.min(regionMax.getBlockX(), cx * 16 + 15) - minX;
+                int regionMaxY = Math.min(regionMax.getBlockY(), cy * 16 + 15) - minY;
+                int regionMaxZ = Math.min(regionMax.getBlockZ(), cz * 16 + 15) - minZ;
+
+                Box box = new Box(regionMinX, regionMinY, regionMinZ, regionMaxX, regionMaxY, regionMaxZ);
+                if (value == StateFlag.State.DENY) {
+                    denyRegions.add(box);
+                } else {
+                    allowRegions.add(box);
+                }
             }
 
             // todo: handle membership
@@ -144,8 +144,8 @@ public class WorldGuardIntegrationImpl {
             // The BUILD flag is implicitly set on every region where
             // PASSTHROUGH is not set to ALLOW
             // todo: handle passthrough
-//            if (FlagValueCalculator.getEffectiveFlagOf(region, Flags.PASSTHROUGH, worldGuardPlayer) != StateFlag.State.ALLOW) {
-//            }
+            //if (FlagValueCalculator.getEffectiveFlagOf(region, Flags.PASSTHROUGH, worldGuardPlayer) != StateFlag.State.ALLOW) {
+            //}
         }
 
         addRegions(sectionBox, denyRegions, allowRegions, finalRegions);
