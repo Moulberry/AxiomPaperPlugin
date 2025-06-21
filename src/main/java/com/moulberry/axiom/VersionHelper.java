a941b38f67183bd0467e45149492cf307a3e23dd
Moulberry <james.jenour@protonmail.com>
2025-06-21 20:51:59 +0800

    Add support for relative marker coordinates (1.21.4)

--- src/main/java/com/moulberry/axiom/VersionHelper.java
+++ src/main/java/com/moulberry/axiom/VersionHelper.java
@@ -2,6 +2,8 @@ package com.moulberry.axiom;
 
 import io.netty.buffer.Unpooled;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.common.custom.DiscardedPayload;
 import net.minecraft.resources.ResourceLocation;
@@ -78,6 +80,10 @@ public class VersionHelper {
 
     public static ResourceLocation createResourceLocation(String namespace, String path) {
         return new ResourceLocation(namespace, path);
+    }
+
+    public static ListTag getList(CompoundTag tag, String key, int type) {
+        return tag.getList(key, type);
     }
 
 }
